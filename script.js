a51a4f3debb7d80d8028ac7d850dd8484de11e21
Jerico <prinbark@gmail.com>
2023-06-08 02:08:07 +0800

    Added Layout: Initial design for tablet devices completed.
    Added feature: Added basic carousel functionality for  projects in the project section.

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ menuLinks.forEach((link) => {
     document.getElementById('menu-toggle').checked = false;
   });
 });
+
+
+const carousel = document.querySelector('.carousel');
+const indicators = document.querySelectorAll('.indicator');
+
+carousel.addEventListener('scroll', () => {
+  const projects = document.querySelectorAll('.project');
+  const projectWidth = projects[0].offsetWidth;
+  const currentProjectIndex = Math.round(carousel.scrollLeft / projectWidth);
+
+  indicators.forEach((indicator, index) => {
+    if (index === currentProjectIndex) {
+      indicator.classList.add('active');
+    } else {
+      indicator.classList.remove('active');
+    }
+  });
+});
